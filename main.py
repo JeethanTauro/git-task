47951308b0b71753285a86231ba53afacfd6641e
Jeethan Shan Tauro <tauroshanjeeth@gmail.com>
2024-06-26 13:26:01 +0530

    we have corrected the code

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ def add(number_a:int, number_b:int):
     """
     This function adds 2 numbers
     """
-    return number_a  + number_b + 1
+   # return number_a  + number_b + 1 here '1' was causing the error
+    return number_a  + number_b # this is the corrected line of code
     
